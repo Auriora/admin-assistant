7f431ef51ea0256a96d24fb288f0933de5f5143b
bcherrington <993834+bcherrington@users.noreply.github.com>
2025-05-26 00:22:55 +0200

    Update Microsoft Graph API configuration to include default values for MS_CLIENT_ID and MS_TENANT_ID in app config.

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///admin_assistant.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Microsoft Graph API OAuth settings
-    MS_CLIENT_ID = os.environ.get('MS_CLIENT_ID')
+    MS_CLIENT_ID = os.environ.get('MS_CLIENT_ID', 'e255aab8-b12b-434f-ba9c-979f06b46fc0')
     MS_CLIENT_SECRET = os.environ.get('MS_CLIENT_SECRET')
-    MS_TENANT_ID = os.environ.get('MS_TENANT_ID')
+    MS_TENANT_ID = os.environ.get('MS_TENANT_ID', 'b7f5c5a6-16cd-4ef1-a3a7-aaa22266ddcb')
     MS_REDIRECT_URI = os.environ.get('MS_REDIRECT_URI', 'http://localhost:5000/ms365/auth/callback') 
